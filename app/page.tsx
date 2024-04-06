a71a2f543caf0405942bb67429d8d3e5fca99196
TomPouv <contact@thomas-pouvreau.fr>
2024-04-06 21:47:19 +0200

    link to next page

--- app/page.tsx
+++ app/page.tsx
@@ -35,13 +35,17 @@ export default function Home() {
         <div className=" inset-0 flex items-center pb-8" aria-hidden="true">
           <div className="w-full border-t border-gray-300" />
         </div>
-        <button
-          type="button"
-          className=" mx-auto w-4/5 inline-flex justify-around gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          <span>Signaler</span>
-          <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-        </button>
+        <div className="mx-auto w-4/5">
+          <Link href="/signal">
+            <button
+              type="button"
+              className="w-full inline-flex justify-around gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <span>Signaler</span>
+              <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+            </button>
+          </Link>
+        </div>
       </div>
     </main>
   );
