2815c99e8519541d00b9f2d0c575e6446f87efb5
TomPouv <contact@thomas-pouvreau.fr>
2024-04-06 21:25:18 +0200

    bim

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,9 @@ const data = [
 export default function Home() {
   return (
     <main className="flex flex-col justify-between h-4/6 mx-4">
-      <h1 className="text-center text-semibold">Patient: Identifiant </h1>
+      <h1 className=" pt-8 text-center font-bold text-2xl">
+        Patient: Identifiant{" "}
+      </h1>
       <div className="bg-white px-4 py-5 sm:px-6">
         <div className=" w-full grid grid-cols-2 gap-4 place-content-around">
           {data.map((cat, index) => (
@@ -28,13 +30,18 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <button
-        type="button"
-        className=" w-full inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Button text
-        <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-      </button>
+      <div className="flex flex-col">
+        <div className=" inset-0 flex items-center pb-8" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <button
+          type="button"
+          className=" mx-auto w-4/5 inline-flex justify-around gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          <span>Signaler</span>
+          <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </button>
+      </div>
     </main>
   );
 }
