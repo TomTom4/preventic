5413b8ba2c05baae30d38673fbc96e38a1f3b5ab
TomPouv <contact@thomas-pouvreau.fr>
2024-04-07 12:25:50 +0200

    fix never issue ( again )

--- app/signal/[id]/_components/SelectDropDown.tsx
+++ app/signal/[id]/_components/SelectDropDown.tsx
@@ -12,7 +12,7 @@ export default function SelectDropDown({
 }: {
   items: Array<{ id: number; value: string; catId: number }>;
 }) {
-  const [selected, setSelected] = useState([{value:null, id:null, catId: null}]);
+  const [selected, setSelected] = useState([]);
   console.log(items);
 
   return (
@@ -25,7 +25,15 @@ export default function SelectDropDown({
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
               {selected.length > 0 ? (
-                <span>{selected.map((item) => item.value).join(", ")}</span>
+                <span>
+                  {selected
+                    .map(
+                      (item: { value: string; id: number; catId: number }) => {
+                        return item.value;
+                      }
+                    )
+                    .join(", ")}
+                </span>
               ) : (
                 <span>Sélectionner...</span>
               )}
