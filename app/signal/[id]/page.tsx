a4acf285a2a38e41d246e214f9d4159cedd93577
TomPouv <contact@thomas-pouvreau.fr>
2024-04-07 10:39:13 +0200

    create redirection

--- app/signal/[id]/page.tsx
+++ app/signal/[id]/page.tsx
@@ -1,5 +1,6 @@
 import SelectDropDown from "./_components/SelectDropDown";
 import TextArea from "./_components/TextArea";
+import Link from "next/link";
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
 export default function Page({ params }: { params: { id: string } }) {
@@ -18,13 +19,18 @@ export default function Page({ params }: { params: { id: string } }) {
           <div className=" inset-0 flex items-center pb-8" aria-hidden="true">
             <div className="w-full border-t border-gray-300" />
           </div>
-          <button
-            type="button"
-            className="w-full inline-flex justify-around gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <span>Envoyer</span>
-            <PaperAirplaneIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-          </button>
+          <Link href="/">
+            <button
+              type="button"
+              className="w-full inline-flex justify-around gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white text-center shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <span>Envoyer</span>
+              <PaperAirplaneIcon
+                className="-mr-0.5 h-5 w-5"
+                aria-hidden="true"
+              />
+            </button>
+          </Link>
         </div>
       </div>
     </div>
