621bef9da509f4386b802e74b4d7065de0a97561
TomPouv <contact@thomas-pouvreau.fr>
2024-04-07 13:05:32 +0200

    please just work

--- app/signal/[id]/_components/NutritionItems.tsx
+++ app/signal/[id]/_components/NutritionItems.tsx
@@ -2,6 +2,6 @@ import prisma from "@/lib/prisma";
 import SelectDropDown from "./SelectDropDown";
 
 export default async function NutritionItems() {
-  const items = await prisma.item.findMany({ where: { catId: 34 } });
+  const items = await prisma.item.findMany({ where: { catId: 35 } });
   return <SelectDropDown items={items} />;
 }
